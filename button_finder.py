6a539db4f30913cc60a68fe4aa500b2c46e2923b
dawnvoid <voiding.voided@gmail.com>
2022-08-10 14:37:41 -0400

    replace url-appending with urllib's implementation

--- button_finder.py
+++ button_finder.py
@@ -6,7 +6,7 @@ import requests.exceptions
 import bs4
 from PIL import ImageFile
 
-FLAG_VERBOSE: bool = False
+FLAG_VERBOSE: bool = True
 
 def get_image_size(image_url: str) -> int:
     result = None
@@ -43,8 +43,8 @@ def get_parent_url(url: str):
 
 def find_links_in_js(js_contents: str) -> list[str]:
     results = []
-    if FLAG_VERBOSE:
-        print(js_contents)
+    # if FLAG_VERBOSE:
+        # print(js_contents)
     return results
 
 def process_page(url: str):
@@ -87,19 +87,21 @@ def process_page(url: str):
             if not link_tag.has_attr('href'):
                 continue
             href: str = link_tag['href'].strip()
-            if href.startswith('https://'):
-                links.append(href)
-            elif href.startswith('http://'):
-                links.append(href)
-            elif href.startswith('/'):
-                href = root_url + href
-                links.append(href)
-            else:
-                # Get parent folder path:
-                parent = get_parent_url(url)
-                # href = url + href
-                href = parent + href
-                links.append(href)
+            # if href.startswith('https://'):
+            #     links.append(href)
+            # elif href.startswith('http://'):
+            #     links.append(href)
+            # elif href.startswith('/'):
+            #     href = root_url + href
+            #     links.append(href)
+            # else:
+            #     # Get parent folder path:
+            #     parent = get_parent_url(url)
+            #     # href = url + href
+            #     href = parent + href
+            #     links.append(href)
+            href = urllib.parse.urljoin(url, href)
+            links.append(href)
         
         images = []
         image_tags = soup.find_all('img')
@@ -107,16 +109,18 @@ def process_page(url: str):
             if not image_tag.has_attr('src'):
                 continue
             src: str = image_tag['src'].strip()
-            if src.startswith('https://'):
-                links.append(src)
-            elif src.startswith('http://'):
-                links.append(src)
-            elif src.startswith('/'):
-                src = root_url + src
-                images.append(src)
-            else:
-                src = url + src
-                images.append(src)
+            # if src.startswith('https://'):
+            #     links.append(src)
+            # elif src.startswith('http://'):
+            #     links.append(src)
+            # elif src.startswith('/'):
+            #     src = root_url + src
+            #     images.append(src)
+            # else:
+            #     src = url + src
+            #     images.append(src)
+            src = urllib.parse.urljoin(url, src)
+            images.append(src)
         
         scripts = []
         script_tags = soup.find_all('script')
@@ -128,22 +132,27 @@ def process_page(url: str):
                 src = script_tag['src'].strip()
             if src == '':
                 continue
-            elif src.startswith('https://'):
-                scripts.append(src)
-            elif src.startswith('http://'):
-                scripts.append(src)
-            elif src.startswith('/'):
-                src = root_url + src
-                scripts.append(src)
-            else:
-                src = url + src
-                scripts.append(src)
+            # elif src.startswith('https://'):
+            #     scripts.append(src)
+            # elif src.startswith('http://'):
+            #     scripts.append(src)
+            # elif src.startswith('/'):
+            #     src = root_url + src
+            #     scripts.append(src)
+            # else:
+            #     src = url + src
+            #     scripts.append(src)
+            src = urllib.parse.urljoin(url, src)
+            scripts.append(src)
             
                     
             # print(f'{script_tag} {s1 = } {s2 = }')
 
         return links, images, scripts
     return [], [], []
+
+def process_image(url: str):
+    pass
 
 def process_site(url: str):
     root_url = get_root_url(url)
@@ -158,96 +167,117 @@ def process_site(url: str):
 
     while len(frontier) > 0:
         # page = frontier.pop()
-        page = frontier.pop(0)
-        checked_pages.append(page)
+        current_url = frontier.pop(0)
+        checked_pages.append(current_url)
 
         # print(f'Checking {page}... {len(checked_pages)}/{len(frontier) + len(checked_pages)}')
-        print(f'Checking {page}...')
+        print(f'Checking {current_url}...')
 
-        try:
-            links, images, scripts = process_page(page)
-        except requests.exceptions.HTTPError as http_error:
-            print(f'ERROR: HTTP error occurred ({http_error})')
-        # except Exception as exception:
-            # print(f'ERROR: Other exception occurred ({exception})')
-        else:
-            for link in links:
-                if not link in total_links:
-                    total_links.append(link)
-            for image in images:
-                if not image in total_images:
-                    total_images.append(image)
-            for script in scripts:
-                if not script in total_scripts:
-                    total_scripts.append(script)
+        def endswith_any(url: str, extensions: list[str]):
+            for extension in extensions:
+                if url.endswith(extension):
+                    return True
+            return False
+        
+        def has_extension(url: str):
+            while len(url) > 0:
+                if url[-1] == '/':
+                    return False
+                elif url[-1] == '.':
+                    return True
+                url = url[:-1]
+            return False
+        
+        if endswith_any(current_url, ['.png', '.jpg', 'jpeg', '.gif', '.bmp', '.tiff', '.webp']):
+            # This is probably an image file.
+            process_image(current_url)
+        elif endswith_any(current_url, ['.html', '.htm', '.com', '.org', '.net']) or not has_extension(current_url):
+            # This is probably an html file.
+            print('HTML FILE')
+            try:
+                links, images, scripts = process_page(current_url)
+            except requests.exceptions.HTTPError as http_error:
+                print(f'ERROR: HTTP error occurred ({http_error})')
+            # except Exception as exception:
+                # print(f'ERROR: Other exception occurred ({exception})')
+            else:
+                for link in links:
+                    if not link in total_links:
+                        total_links.append(link)
+                for image in images:
+                    if not image in total_images:
+                        total_images.append(image)
+                for script in scripts:
+                    if not script in total_scripts:
+                        total_scripts.append(script)
 
-            if FLAG_VERBOSE:
-                print('\tLinks:')
-                if len(links) == 0:
-                    print('\tNone')
-                else:
-                    for link in links:
-                        print(f'\t{link}')
-                print()
+                if FLAG_VERBOSE:
+                    print('\tLinks:')
+                    if len(links) == 0:
+                        print('\tNone')
+                    else:
+                        for link in links:
+                            print(f'\t{link}')
+                    print()
 
-                print('\tImages:')
-                if len(images) == 0:
-                    print('\tNone')
-                else:
-                    for image in images:
-                        print(f'\t{image}')
-                print()
+                    print('\tImages:')
+                    if len(images) == 0:
+                        print('\tNone')
+                    else:
+                        for image in images:
+                            print(f'\t{image}')
+                    print()
 
-                print('\tScripts:')
-                if len(scripts) == 0:
-                    print('\tNone')
-                else:
-                    for script in scripts:
-                        print(f'\t{script}')
-                print()
+                    print('\tScripts:')
+                    if len(scripts) == 0:
+                        print('\tNone')
+                    else:
+                        for script in scripts:
+                            print(f'\t{script}')
+                    print()
 
-            # Add pages to the frontier.
-            for link in links:
-                # If the link isn't an html page, ignore it.
-                if not (link.endswith('.html') or link.endswith('.htm') or link.endswith('.js')):
-                    continue
-                # If the link is external, ignore it.
-                if not link.startswith(root_url):
-                    # print(f'{link} is external')
-                    continue
-                # If link was already checked, ignore it.
-                elif link in checked_pages:
-                    # print(f'{link} was already visited')
-                    continue
-                frontier.append(link)
+                # Add pages to the frontier.
+                for link in links:
+                    # If the link isn't an html page, ignore it.
+                    # if not (link.endswith('.html') or link.endswith('.htm') or link.endswith('.js')):
+                        # continue
+                    # If the link is external, ignore it.
+                    if not link.startswith(root_url):
+                        # print(f'{link} is external')
+                        continue
+                    # If link was already checked, ignore it.
+                    elif link in checked_pages:
+                        # print(f'{link} was already visited')
+                        continue
+                    frontier.append(link)
 
-            # TODO: Do this in parallel when there's a lot of images, it's too slow.
-            if FLAG_VERBOSE:
-                print('\tPotential buttons:')
-            for image in images:
-                image_size = get_image_size(image)
-                if image_size is None:
-                    # Image size is unknown
-                    continue
-                elif image_size >= 500000: # < 5kb is reasonable right?
-                    # Image is too big
-                    continue
-                image_dimensions = get_image_dimensions(image)
-                if image_dimensions == (88, 31):
-                    if image not in buttons:
-                        buttons.append(image)
-                    if FLAG_VERBOSE:
-                        print(f'\t{image}')
-            
-            for script in scripts:
-                response = requests.get(script)
-                js = response.text
-                find_links_in_js(js)
+                # TODO: Do this in parallel when there's a lot of images, it's too slow.
+                if FLAG_VERBOSE:
+                    print('\tPotential buttons:')
+                for image in images:
+                    image_size = get_image_size(image)
+                    if image_size is None:
+                        # Image size is unknown
+                        continue
+                    elif image_size >= 500000: # < 5kb is reasonable right?
+                        # Image is too big
+                        continue
+                    image_dimensions = get_image_dimensions(image)
+                    if image_dimensions == (88, 31):
+                        if image not in buttons:
+                            buttons.append(image)
+                        if FLAG_VERBOSE:
+                            print(f'\t{image}')
+                
+                for script in scripts:
+                    response = requests.get(script)
+                    js = response.text
+                    find_links_in_js(js)
 
-            # print(f'Possible buttons in {page}')
-            # for button in buttons:
-            #     print(f'{button}')
-            # print()
+                # print(f'Possible buttons in {page}')
+                # for button in buttons:
+                #     print(f'{button}')
+                # print()
     print('Done!')
     print()
     
@@ -276,12 +306,14 @@ def process_site(url: str):
     print()
 
 
-url = 'https://dawnvoid.neocities.org'
+# url = 'https://dawnvoid.neocities.org'
 # url = 'https://dawnvoid.neocities.org/home.html'
 # url = 'https://dawnvoid.neocities.org/page/media/media.html'
 # url = 'https://dawnvoid.neocities.org/page/buttons/buttons.html'
 # url = 'https://koyo.neocities.org/koy19/home.html'
 # url = 'https://fauux.neocities.org'
+# url = 'https://scarbyte.com'
+url = 'https://hosma.neocities.org'
 # url = 'https://google.com'
 # url = 'https://wikipedia.org/'
 # url = 'https://dawnvoid.neocities.org/assets/angel.gif'
@@ -302,3 +334,11 @@ process_site(url)
 
 # https?:\/\/|\/?[A-Za-z0-9\.\-\_]+\.[A-Za-z0-9\.\-\_]+
 # ["'](https:\/\/[A-Za-z0-9\.\-\_\/]+\.[A-Za-z0-9\.\-\_\/]+\.[A-Za-z0-9]+)['"]
+
+url = 'https://dawnvoid.neocities.org/page/./'
+result = urllib.parse.urljoin(url, './terminal/terminal.html/#')
+print(result)
+
+url = 'https://dawnvoid.neocities.org/page/./terminal/terminal.html'
+result = urllib.parse.urljoin(url, 'test.gif')
+print(result)
